f0f1414b6ed576a5ca3242b630be459e75a48462
EarlyMorningCoffee <95649405+EarlyMorningCoffee@users.noreply.github.com>
2022-07-31 23:46:50 +0000

    Add files via upload

--- main.java
+++ main.java
@@ -14,34 +14,33 @@ public class main {
          Scanner reader = new Scanner (System.in);
          
          while (true) {
-        	 try { 
-        	 int input = reader.nextInt();
+        	 try {
+        		 int input = reader.nextInt();
         	 
-             if (input == 1) {
-            	 int speed = fan.getSpeed();
-                 if (speed == 2) {
-                     speed = 0;
-                     System.out.println("The fan's speed return to off.");
-                 }else {
-                     speed++;
-                     System.out.println("The fan's speed increases.");
-                 }
-                 fan.setSpeed(speed);
-                 System.out.printf("The fan's current speed is %d and its direction is %s \n",fan.getSpeed(),fan.getDirection());
-             }else if (input == 2) {
-            	 System.out.println("pull 2");
-            	 String direction = fan.getDirection();
-            	 System.out.println("Reversed the fan's direction.");
-            	 if (direction == "Forward") {
-            		 direction = "Backward";
-            	 }else {
-            		 direction = "Forward";
-            	 }
-            	 fan.setDirection(direction);
-            	 System.out.printf("The fan's current speed is %d and its direction is %s \n",fan.getSpeed(),fan.getDirection());
-             }else {
-            	 System.out.println("Not a valid pull cord, please choose again.");
-             }
+	             if (input == 1) {
+	            	 int speed = fan.getSpeed();
+	                 if (speed == 2) {
+	                     speed = 0;
+	                     System.out.println("The fan's speed return to off.");
+	                 }else {
+	                     speed++;
+	                     System.out.println("The fan's speed increases.");
+	                 }
+	                 fan.setSpeed(speed);
+	                 System.out.printf("The fan's current speed is %d and its direction is %s \n",fan.getSpeed(),fan.getDirection());
+	             }else if (input == 2) {
+	            	 String direction = fan.getDirection();
+	            	 System.out.println("Reversed the fan's direction.");
+	            	 if (direction == "Forward") {
+	            		 direction = "Backward";
+	            	 }else {
+	            		 direction = "Forward";
+	            	 }
+	            	 fan.setDirection(direction);
+	            	 System.out.printf("The fan's current speed is %d and its direction is %s \n",fan.getSpeed(),fan.getDirection());
+	             }else {
+	            	 System.out.println("Not a valid pull cord, please choose again.");
+	             }
         	 }catch (Exception e) { 
         		 System.out.println("Invalid input type (must be an integer), please try again.");
                  reader.nextLine();
